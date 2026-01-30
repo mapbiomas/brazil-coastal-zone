d9b364999807cad566d83d7ddc6297ac3b05c01b
Júlia Cansado <156091433+julcansado@users.noreply.github.com>
2026-01-30 15:00:19 -0300

    Implement coastal reef classification in 2-classification.js
    Added classification logic for coastal reefs using Landsat data.

---
+++ Mapbiomas10/REEFS/2-classification.js
@@ -0,0 +1,398 @@
+var complete_grid = 
+    /* color: #d63000 */
+    /* shown: false */
+    ee.FeatureCollection(
+        [ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-39.282999944298915, -17.83865602315809],
+                  [-39.282999944298915, -18.04508403292794],
+                  [-39.085246038048915, -18.04508403292794],
+                  [-39.085246038048915, -17.83865602315809]]], null, false),
+            {
+              "system:index": "0"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-39.07288641890829, -17.592781091144904],
+                  [-39.07288641890829, -17.90537430088679],
+                  [-38.91633124312704, -17.90537430088679],
+                  [-38.91633124312704, -17.592781091144904]]], null, false),
+            {
+              "system:index": "1"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-39.093485784142665, -17.407407819403645],
+                  [-39.093485784142665, -17.544947542848927],
+                  [-38.953410100548915, -17.544947542848927],
+                  [-38.953410100548915, -17.407407819403645]]], null, false),
+            {
+              "system:index": "2"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-38.766642522423915, -17.892913052054343],
+                  [-38.766642522423915, -18.033998568489626],
+                  [-38.640299748986415, -18.033998568489626],
+                  [-38.640299748986415, -17.892913052054343]]], null, false),
+            {
+              "system:index": "3"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-38.182860764408744, -12.751312113718917],
+                  [-38.182860764408744, -12.78479544516809],
+                  [-38.16500798120562, -12.78479544516809],
+                  [-38.16500798120562, -12.751312113718917]]], null, false),
+            {
+              "system:index": "4"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-38.02218571558062, -12.507420668097954],
+                  [-38.02218571558062, -12.59321118685225],
+                  [-37.943908127689994, -12.59321118685225],
+                  [-37.943908127689994, -12.507420668097954]]], null, false),
+            {
+              "system:index": "5"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-37.817565354252494, -12.225142040822501],
+                  [-37.817565354252494, -12.303646077181034],
+                  [-37.76950016870562, -12.303646077181034],
+                  [-37.76950016870562, -12.225142040822501]]], null, false),
+            {
+              "system:index": "6"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-36.87934836460312, -10.657607881661997],
+                  [-36.87934836460312, -10.758811125917319],
+                  [-36.78733786655624, -10.758811125917319],
+                  [-36.78733786655624, -10.657607881661997]]], null, false),
+            {
+              "system:index": "7"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-36.36848410679062, -10.371359052947376],
+                  [-36.36848410679062, -10.45239975187713],
+                  [-36.30119284702499, -10.45239975187713],
+                  [-36.30119284702499, -10.371359052947376]]], null, false),
+            {
+              "system:index": "8"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-36.020853240140944, -9.869982855449177],
+                  [-36.020853240140944, -9.978202229184847],
+                  [-35.900003630765944, -9.978202229184847],
+                  [-35.900003630765944, -9.869982855449177]]], null, false),
+            {
+              "system:index": "9"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-35.79151364053157, -9.625005806502672],
+                  [-35.79151364053157, -9.688635893399203],
+                  [-35.715982634672194, -9.688635893399203],
+                  [-35.715982634672194, -9.625005806502672]]], null, false),
+            {
+              "system:index": "10"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-35.504495818265944, -9.310742727427373],
+                  [-35.504495818265944, -9.415077373184173],
+                  [-35.416605193265944, -9.415077373184173],
+                  [-35.416605193265944, -9.310742727427373]]], null, false),
+            {
+              "system:index": "11"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-35.39338705576801, -9.22664489829365],
+                  [-35.39338705576801, -9.29848028824394],
+                  [-35.33296225108051, -9.29848028824394],
+                  [-35.33296225108051, -9.22664489829365]]], null, false),
+            {
+              "system:index": "12"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-35.284897065533634, -9.05309552281633],
+                  [-35.284897065533634, -9.110050724617885],
+                  [-35.24232504404926, -9.110050724617885],
+                  [-35.24232504404926, -9.05309552281633]]], null, false),
+            {
+              "system:index": "13"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-35.202499604596134, -8.924235081600713],
+                  [-35.202499604596134, -9.032752189911799],
+                  [-35.131088471783634, -9.032752189911799],
+                  [-35.131088471783634, -8.924235081600713]]], null, false),
+            {
+              "system:index": "14"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-35.12422201670551, -8.624289953964832],
+                  [-35.12422201670551, -8.732895320238056],
+                  [-35.018478608502384, -8.732895320238056],
+                  [-35.018478608502384, -8.624289953964832]]], null, false),
+            {
+              "system:index": "15"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-34.998975466564744, -8.36581804827356],
+                  [-34.998975466564744, -8.421519865589737],
+                  [-34.949536990002244, -8.421519865589737],
+                  [-34.949536990002244, -8.36581804827356]]], null, false),
+            {
+              "system:index": "16"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-34.902845095470994, -8.055917734570878],
+                  [-34.902845095470994, -8.127977471725742],
+                  [-34.83830041773662, -8.127977471725742],
+                  [-34.83830041773662, -8.055917734570878]]], null, false),
+            {
+              "system:index": "17"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-34.98659458521588, -6.483195653203572],
+                  [-34.98659458521588, -6.483195653203572],
+                  [-34.98659458521588, -6.483195653203572],
+                  [-34.98659458521588, -6.483195653203572]]], null, false),
+            {
+              "system:index": "18"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-34.99895420435651, -6.420424352003671],
+                  [-34.99895420435651, -6.651002069455337],
+                  [-34.94676914576276, -6.651002069455337],
+                  [-34.94676914576276, -6.420424352003671]]], null, false),
+            {
+              "system:index": "19"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-35.10469761255963, -6.139224211121515],
+                  [-35.10469761255963, -6.255271776100209],
+                  [-35.03328647974713, -6.255271776100209],
+                  [-35.03328647974713, -6.139224211121515]]], null, false),
+            {
+              "system:index": "20"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-38.04709702575836, -4.19499599102201],
+                  [-38.04709702575836, -4.288124000010203],
+                  [-37.93036728943024, -4.288124000010203],
+                  [-37.93036728943024, -4.19499599102201]]], null, false),
+            {
+              "system:index": "21"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-37.94272690857086, -4.315512442077026],
+                  [-37.94272690857086, -4.378502099385222],
+                  [-37.84384995544586, -4.378502099385222],
+                  [-37.84384995544586, -4.315512442077026]]], null, false),
+            {
+              "system:index": "22"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-38.40689927185211, -3.8086728942537063],
+                  [-38.40689927185211, -3.8826636249830617],
+                  [-38.33136826599274, -3.8826636249830617],
+                  [-38.33136826599274, -3.8086728942537063]]], null, false),
+            {
+              "system:index": "23"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-38.23523789489899, -3.981307824631391],
+                  [-38.23523789489899, -4.081310015128145],
+                  [-38.13910752380524, -4.081310015128145],
+                  [-38.13910752380524, -3.981307824631391]]], null, false),
+            {
+              "system:index": "24"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-38.72550278747711, -3.611333512019008],
+                  [-38.72550278747711, -3.7045270429608954],
+                  [-38.62250596130524, -3.7045270429608954],
+                  [-38.62250596130524, -3.611333512019008]]], null, false),
+            {
+              "system:index": "25"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-39.15218164365975, -3.315189454517988],
+                  [-39.15218164365975, -3.422121053653254],
+                  [-39.012105960066, -3.422121053653254],
+                  [-39.012105960066, -3.315189454517988]]], null, false),
+            {
+              "system:index": "26"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-41.149447689857176, -2.841011752442997],
+                  [-41.149447689857176, -2.9178189494981845],
+                  [-40.97229314884155, -2.9178189494981845],
+                  [-40.97229314884155, -2.841011752442997]]], null, false),
+            {
+              "system:index": "27"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-41.797641049232176, -2.7559692580623785],
+                  [-41.797641049232176, -2.8067211671662156],
+                  [-41.7550690277478, -2.8067211671662156],
+                  [-41.7550690277478, -2.7559692580623785]]], null, false),
+            {
+              "system:index": "28"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-42.72539903384978, -2.551813209981505],
+                  [-42.72539903384978, -2.6423573119194104],
+                  [-42.60042955142791, -2.6423573119194104],
+                  [-42.60042955142791, -2.551813209981505]]], null, false),
+            {
+              "system:index": "29"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-42.57708360416228, -2.6615627953164056],
+                  [-42.57708360416228, -2.732894810833017],
+                  [-42.49880601627166, -2.732894810833017],
+                  [-42.49880601627166, -2.6615627953164056]]], null, false),
+            {
+              "system:index": "30"
+            }),
+        ee.Feature(
+            ee.Geometry.Polygon(
+                [[[-33.89795373406123, -3.7775767418291277],
+                  [-33.89795373406123, -3.9639181967391295],
+                  [-33.70294640984248, -3.9639181967391295],
+                  [-33.70294640984248, -3.7775767418291277]]], null, false),
+            {
+              "system:index": "31"
+            })]),
+    geometry = 
+    /* color: #98ff00 */
+    /* shown: false */
+    ee.Geometry.Polygon(
+        [[[-43.216573015888244, -2.902995232109251],
+          [-39.94224471496497, -3.428847500614536],
+          [-36.41515562622076, -5.546999651735299],
+          [-35.7105986240808, -5.734170665166369],
+          [-35.53140561138849, -7.755737205155455],
+          [-34.51860599141792, -8.094822097176303],
+          [-34.77066161138822, -5.241840076509022],
+          [-33.18913346993959, -3.8675425753462576],
+          [-33.98630655334582, -3.231174834437945],
+          [-35.21175588090587, -4.3887198801485345],
+          [-36.60295883908884, -4.72257254764427],
+          [-38.59259417701504, -3.0887959344153173],
+          [-40.11380058210988, -2.328475896437984],
+          [-41.919814365285745, -2.3230121595782216],
+          [-43.93770291793936, -1.6580473162923328],
+          [-45.240914705550274, -0.794510406174578],
+          [-44.87814592548745, -3.505893435652653]]]);
+
+
+var col9_class = ee.FeatureCollection("projects/solved-mb10/assets/LANDSAT/CORAL_REEFS/mb9_class");
+
+var scale = 30;
+// Map.setCenter(-39.09401, -16.91254,13)
+var version = '1';
+var oceanMask = ee.Image('users/julcansado/Solved/Corais/oceanMask_v1');
+var year = '2020_2023';
+
+// var parametros = 'C_BL33_NC_BG25';
+// var filterCoral = ee.Filter.lte('Blue',33);
+// var filterNotCoral = ee.Filter.gte('Blue',25);
+
+var parametros = 'C_BL30_NC_GBNDVIL25';
+var filterCoral = ee.Filter.gte('Blue',30);
+var filterNotCoral = ee.Filter.lte('GBNDVI',70);
+
+var samples = ee.FeatureCollection('users/julcansado/Solved/Corais/Samples/samples_v1'); 
+samples = ee.FeatureCollection("projects/ee-cansado/assets/MB10/samples_v1_2025");
+// Map.addLayer(grids);
+// Map.addLayer(samples);
+
+var classifiedCollection = ee.ImageCollection([]);
+
+var  grids = ee.FeatureCollection("users/julcansado/Solved/Corais/GRID_CORAIS_V5");
+
+var geo = 'geo1';
+var region_to_process = grids.merge(complete_grid).filterBounds(geometry);
+Map.addLayer(region_to_process,{},'region_to_process');
+var region_geometry = region_to_process.geometry(1);
+var grid = region_geometry.dissolve();
+Map.addLayer(grid);
+var grid_raster = ee.Image(0).paint(region_to_process,1)
+Map.addLayer(grid_raster,{},'grid_raster');
+// var landsat = landsat8.merge(landsat9).merge(landsat5);
+var landsat = ee.Image("users/julcansado/Solved/Corais/Landsat_v2/mosaic_2020-2023").toByte().clip(grid)
+Map.addLayer(landsat)
+// print(landsat);
+
+
+var samplesBounds = samples.filterBounds(grid);
+// Map.addLayer(samplesBounds,{},'samplesFilterBounds');
+
+var samplesTrain = landsat.sampleRegions({collection:samplesBounds, scale: scale, tileScale:1, geometries:true});
+var samplesCoral = samplesTrain.filter(ee.Filter.eq('coral',1)).filter(filterCoral);
+var samplesNotCoral = samplesTrain.filter(ee.Filter.eq('coral',0)).filter(filterNotCoral);
+var samplesFilter = samplesCoral.merge(samplesNotCoral);
+var samplesFinal = samplesFilter.filter(ee.Filter.notNull(['Blue'])).sort('random').limit(15000);
+// Map.addLayer(samplesTrain,{},'samplesTrain');
+// print(samplesFinal.size());
+
+
+// var mapImage = collectionPrep.map(function(image){
+
+var randomForest = ee.Classifier.smileRandomForest(100).train({features:samplesFinal, classProperty:'coral', inputProperties:['Blue','Green','Red','NIR','SWIR1','GBNDVI','GRNDVI','GARI','CI']});
+randomForest = randomForest.setOutputMode('PROBABILITY');
+// print('randomForest:',randomForest.explain());
+
+var classify = landsat.classify(randomForest,'coral').toByte();
+// Map.addLayer(classify,{min:0,max:1},'classified');
+
+var classification = ee.Image([classify.copyProperties(landsat)
+                                                .set('version',version)
+                                                .set('parametros',parametros)
+                                                .set('year',year)
+                                                ]);
+
+// return classification;
+
+// });
+// print(mapImage.limit(1));
+
+// var mean = mapImage.mean().copyProperties(mapImage.first());
+// print(mean);
+Export.image.toAsset({image:classification, 
+                      description:parametros+'_v'+version+'_'+ year, 
+                      assetId:'projects/solved-mb10/assets/LANDSAT/CORAL_REEFS/raw_v1/'+parametros+'_v'+version+'_'+geo+'_' +year, 
+                      region: grid, 
+                      scale:scale, 
+                      maxPixels:1e13});
+
+// Export.table.toAsset({collection:new_grids, description:'landsat_grids_'+version+"_"+year, assetId:'users/pc01solved/Corais/landsat_grids_'+version+"_"+year})
+
+
